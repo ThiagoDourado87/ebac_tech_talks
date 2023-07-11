a28e7f4f43d04935fd6fc43b38666f969a56ab28
Thiago Dourado <thiagodourado@gmail.com>
2023-07-10 21:42:30 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento =new Date("Dec 12, 2023 19:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras =setInterval(function(){
+    const agora=new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMS = 1000*60*60*24;
+    const horaEmMS = 1000*60*60;
+    const minutoEmMS= 1000*60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMS)/horaEmMS);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMS)/minutoEmMS);
+    const segudosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMS)/1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento} d ${horasAteOEvento}h ${minutosAteOEvento}min ${segudosAteOEvento}s `
+
+    if (distanciaAteOEvento <0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento Expirado`;
+    }
+
+},1000)
